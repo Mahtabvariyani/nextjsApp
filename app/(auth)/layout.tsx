80989c48679d682e3458e73d2a6f4c0db961d5dd
Mah <mahtabvariyani@gmail.com>
2023-09-24 00:59:48 +0200

    Semi done after that is boarding

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+
 
 import "../globals.css";
 
 export const metadata = {
   title: "Coin",
-  description: "A Next.js 13 Meta Threads Application",
+  description: "A Next.js 13 Meta Coins Application",
 };
-
 
 const inter = Inter({ subsets: ["latin"] });
 
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,20 @@
 
 export default function RightSidebar() {
   return (
-    <div>
-      Rightside
-    </div>
+    <section className="custum-scrollbar rightsidebar" >
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Community
+        </h3>
+
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Users
+        </h3>
+
+      </div>
+      
+    </section>
   )
 }
